b889b01ad7bb282bb9b8b95f7101bd40023224ad
Motoki Kimura <motokimura1@gmail.com>
2017-11-25 00:33:59 +0900

    Modify some variable names in PrimaryCaps class

--- primary_caps.py
+++ primary_caps.py
@@ -14,55 +14,65 @@ from utils import squash
 
 
 class ConvUnit(nn.Module):
-	def __init__(self):
+	def __init__(self, out_channels):
 		super(ConvUnit, self).__init__()
 
 		self.conv0 = nn.Conv2d(
 			in_channels=256,
-			out_channels=8,
+			out_channels=out_channels,
 			kernel_size=9,
 			stride=2,
 			bias=True
 		)
 
 	def forward(self, x):
-		return self.conv0(x)
+		# x: [batch_size, 256, 20, 20]
+
+		h = self.conv0(x)
+		# h: [batch_size, out_channels, 6, 6]
+
+		return h
 
 
 class PrimaryCaps(nn.Module):
 	def __init__(self):
 		super(PrimaryCaps, self).__init__()
 
-		
+		self.capsule_units = 32
+		self.capsule_size = 8
 
 		def create_conv_unit(unit_idx):
-				unit = ConvUnit()
+				unit = ConvUnit(self.capsule_units)
 				self.add_module("unit_" + str(unit_idx), unit)
 				return unit
 
-		self.units = [create_conv_unit(i) for i in range(32)]
+		self.conv_units = [create_conv_unit(i) for i in range(self.capsule_units)]
 
 	def forward(self, x):
+		# x: [batch_size, 256, 20, 20]
 		batch_size = x.size(0)
 
 		u = []
-		for i in range(32):
-			u_i = self.units[i](x)
+		for i in range(self.capsule_units):
+			u_i = self.conv_units[i](x)
+			# u_i: [batch_size, capsule_size=8, 6, 6]
 
-			# (batch_size, 8, 36, 1)
-			u_i = u_i.view(batch_size, 8, -1, 1)
+			u_i = u_i.view(batch_size, self.capsule_size, -1, 1)
+			# u_i: [batch_size, capsule_size=8, 36, 1]
 
 			u.append(u_i)
-		# u: (batch_size, 8, 36, 1) x 32
+		# u: [batch_size, capsule_size=8, 36, 1] x capsule_units=32
 
-		# (batch_size, 8, 36, 32)
 		u = torch.cat(u, dim=3)
+		# u: [batch_size, capsule_size=8, 36, capsule_units=32]
 
-		# Flatten to (batch_size, 8, 1152=36*32).
-		u = u.view(batch_size, 8, -1)
+		u = u.view(batch_size, self.capsule_size, -1)
+		# u: [batch_size, capsule_size=8, 1152=36*32]
 
-		# Transpose to (batch_size, 1152, 8)
 		u = u.transpose(1, 2)
+		# u: [batch_size, 1152, capsule_size=8]
 
-		# Return squashed outputs.
-		return squash(u, dim=2)
+		u_squashed = squash(u, dim=2)
+		# u_squashed: [batch_size, 1152, capsule_size=8]
+
+		return u_squashed
