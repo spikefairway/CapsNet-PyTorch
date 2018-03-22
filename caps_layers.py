09f42d2b7a619e323382cd70ddc04afedfc4201f
spikefairway <keisu-ma@dl-box>
2018-03-22 14:40:34 +0900

    Remove ConvUnit class.

--- caps_layers.py
+++ caps_layers.py
@@ -12,39 +12,24 @@ import torch.nn.functional as F
 
 from squash import squash
 
-
-class ConvUnit(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super(ConvUnit, self).__init__()
-
-        self.conv = nn.Conv2d(
-            in_channels=in_channels,
-            out_channels=out_channels,
-            kernel_size=9,
-            stride=2,
-            bias=True
-        )
-
-    def forward(self, x):
-        # x: [batch_size, in_channels=256, 20, 20]
-
-        h = self.conv(x)
-        # h: [batch_size, out_channels=8, 6, 6]
-
-        return h
-
-
 class PrimaryCaps(nn.Module):
-    def __init__(self):
+    """
+    PrimaryCaps layers.
+    """
+    def __init__(self, in_channels, out_capsules, out_capsule_dim,
+                 kernel_size=9, stride=2, is_relu=False):
         super(PrimaryCaps, self).__init__()
 
-        self.conv1_out = 256 # out_channels of Conv1, a ConvLayer just before PrimaryCaps
-        self.capsule_units = 32
-        self.capsule_size = 8
+        self.in_channels = in_channels
+        self.out_capsules = out_capsules
+        self.out_capsule_dim = out_capsule_dim
 
-        self.capsules = ConvUnit(
-            in_channels=self.conv1_out,
-            out_channels=self.capsule_size * self.capsule_units
+        self.capsules = nn.Conv2d(
+                in_channels=in_channels,
+                out_channels=out_capsules * out_capsule_dim,
+                kernel_size=kernel_size,
+                stride=stride,
+                bias=True
         )
 
     def forward(self, x):
@@ -54,14 +39,14 @@ class PrimaryCaps(nn.Module):
         u = self.capsules(x)
         # u: [batch_size, 8*32, 6, 6]
 
-        u = u.view(batch_size, self.capsule_size, -1)
-        # u: [batch_size, capsule_size=8, 1152=6*6*32]
+        u = u.view(batch_size, self.out_capsule_dim, -1)
+        # u: [batch_size, out_capsule_dim=8, 1152=6*6*32]
 
         u = u.transpose(1, 2)
-        # u: [batch_size, 1152, capsule_size=8]
+        # u: [batch_size, 1152, out_capsule_dim=8]
 
         u_squashed = squash(u, dim=2)
-        # u_squashed: [batch_size, 1152, capsule_size=8]
+        # u_squashed: [batch_size, 1152, out_capsule_dim=8]
 
         return u_squashed
 
@@ -73,19 +58,19 @@ class DigitCaps(nn.Module):
         self.gpu = gpu
 
         self.in_capsules = 1152
-        self.in_capsule_size = 8
+        self.in_out_capsule_dim = 8
         self.out_capsules = 10
-        self.out_capsule_size = 16
+        self.out_capsule_dim = 16
 
         self.W = nn.Parameter(
             torch.Tensor(
                 self.in_capsules, 
                 self.out_capsules, 
-                self.out_capsule_size, 
-                self.in_capsule_size
+                self.out_capsule_dim, 
+                self.in_out_capsule_dim
             )
         )
-        # W: [in_capsules, out_capsules, out_capsule_size, in_capsule_size] = [1152, 10, 16, 8]
+        # W: [in_capsules, out_capsules, out_capsule_dim, in_out_capsule_dim] = [1152, 10, 16, 8]
         self.reset_parameters()
 
     def reset_parameters(self):
@@ -94,21 +79,21 @@ class DigitCaps(nn.Module):
 
     # FIXME, write in an easier way to understand, some tensors have some redundant dimensions.
     def forward(self, x):
-        # x: [batch_size, in_capsules=1152, in_capsule_size=8]
+        # x: [batch_size, in_capsules=1152, in_out_capsule_dim=8]
         batch_size = x.size(0)
 
         x = torch.stack([x] * self.out_capsules, dim=2)
-        # x: [batch_size, in_capsules=1152, out_capsules=10, in_capsule_size=8]
+        # x: [batch_size, in_capsules=1152, out_capsules=10, in_out_capsule_dim=8]
 
         W = torch.cat([self.W.unsqueeze(0)] * batch_size, dim=0)
-        # W: [batch_size, in_capsules=1152, out_capsules=10, out_capsule_size=16, in_capsule_size=8]
+        # W: [batch_size, in_capsules=1152, out_capsules=10, out_capsule_dim=16, in_out_capsule_dim=8]
 
         # Transform inputs by weight matrix `W`.
         u_hat = torch.matmul(W, x.unsqueeze(4)) # matrix multiplication
-        # u_hat: [batch_size, in_capsules=1152, out_capsules=10, out_capsule_size=16, 1]
+        # u_hat: [batch_size, in_capsules=1152, out_capsules=10, out_capsule_dim=16, 1]
 
         u_hat_detached = u_hat.detach()
-        # u_hat_detached: [batch_size, in_capsules=1152, out_capsules=10, out_capsule_size=16, 1]
+        # u_hat_detached: [batch_size, in_capsules=1152, out_capsules=10, out_capsule_dim=16, 1]
         # In forward pass, `u_hat_detached` = `u_hat`, and 
     # in backward, no gradient can flow from `u_hat_detached` back to `u_hat`.
 
@@ -127,18 +112,18 @@ class DigitCaps(nn.Module):
             if iteration == self.routing_iters - 1:
                 # Apply routing `c_ij` to weighted inputs `u_hat`.
                 s_j = (c_ij * u_hat).sum(dim=1, keepdim=True) # element-wise product
-                # s_j: [batch_size, 1, out_capsules=10, out_capsule_size=16, 1]
+                # s_j: [batch_size, 1, out_capsules=10, out_capsule_dim=16, 1]
     
                 v_j = squash(s_j, dim=3)
-                # v_j: [batch_size, 1, out_capsules=10, out_capsule_size=16, 1]
+                # v_j: [batch_size, 1, out_capsules=10, out_capsule_dim=16, 1]
 
             else:
                 # Apply routing `c_ij` to weighted inputs `u_hat`.
                 s_j = (c_ij * u_hat_detached).sum(dim=1, keepdim=True) # element-wise product
-                # s_j: [batch_size, 1, out_capsules=10, out_capsule_size=16, 1]
+                # s_j: [batch_size, 1, out_capsules=10, out_capsule_dim=16, 1]
     
                 v_j = squash(s_j, dim=3)
-                # v_j: [batch_size, 1, out_capsules=10, out_capsule_size=16, 1]
+                # v_j: [batch_size, 1, out_capsules=10, out_capsule_dim=16, 1]
     
                 # Compute inner products of 2 16D-vectors, `u_hat` and `v_j`.
                 u_vj1 = torch.matmul(u_hat_detached.transpose(3, 4), v_j).squeeze(4)
@@ -148,4 +133,4 @@ class DigitCaps(nn.Module):
                 # Update b_ij (routing).
                 b_ij = b_ij + u_vj1
 
-        return v_j.squeeze(4).squeeze(1) # [batch_size, out_capsules=10, out_capsule_size=16]
+        return v_j.squeeze(4).squeeze(1) # [batch_size, out_capsules=10, out_capsule_dim=16]
